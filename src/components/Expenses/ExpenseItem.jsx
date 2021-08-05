86431a8a3befb0d414f072daa702914e5dffd788
denny6389 <50681844+denny6389@users.noreply.github.com>
2021-08-05 23:35:14 +0900

    Rendering lists

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,20 +4,19 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const [titleSate, setTitle] = useState(props.title);
+  //const [titleState, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("Updated");
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated");
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{titleSate}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
